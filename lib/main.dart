9c2e5d4223b117374ef7a8a994c212469f4cf0ed
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-02 20:57:30 +0800

    Testing Pt:2

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'The Best Mobile App'),
     );
   }
 }
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'LOL:',
+              'Counter:',
             ),
             Text(
               '$_counter',
