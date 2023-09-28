1e270903858ba062568ba9e17c881823a2fcf45b
Alyssa De Conceicao <alyssavictoria.deconceicao@gmail.com>
2023-09-28 15:11:24 +0800

    Included Popup

--- lib/Recycling/Pages/pop_up.dart
+++ lib/Recycling/Pages/pop_up.dart
@@ -11,12 +11,79 @@ class Popup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('Pop Up'),
-        // ),
-        body: const Center(
-          child: Text('Pop Up'),
-        ),
-        bottomNavigationBar: const BottomBar());
+      appBar: AppBar(
+        title: const Text('Pop Up'),
+      ),
+      body: const Center(
+        child: Text('Pop Up'),
+      ),
+      bottomNavigationBar: const BottomBar(),
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Add Item'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Item Name',
+                      ),
+                      onChanged: (value) {
+                        // Do something with the user's input
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Quantity',
+                      ),
+                      keyboardType: TextInputType.number,
+                      onChanged: (value) {
+                        // Do something with the user's input
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Do something when the green button is pressed
+                          },
+                          style:
+                              ElevatedButton.styleFrom(primary: Colors.green),
+                          child: const Text('Add'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Do something when the red button is pressed
+                          },
+                          style: ElevatedButton.styleFrom(primary: Colors.red),
+                          child: const Text('Cancel'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: const Text('Add Item'),
+      ),
+    );
   }
 }
